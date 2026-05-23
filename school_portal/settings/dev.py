9f534506b2b9d2b1e3bbc5bc94619d07b63c9b18
Muhammad Abayomi Ibraheem <mmabayomi2208@gmail.com>
2026-05-23 11:40:13 +0100

    fixed up settings.py

--- school_portal/settings/dev.py
+++ school_portal/settings/dev.py
@@ -7,13 +7,14 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+DATABASE_URL = f'postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}'
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
