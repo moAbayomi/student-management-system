5ea508fd6dcbc90120c46bfef6c4ea4b71efeb51
Muhammad Abayomi Ibraheem <mmabayomi2208@gmail.com>
2026-05-23 12:01:33 +0100

    fixed up other things with env

--- school_portal/settings/dev.py
+++ school_portal/settings/dev.py
@@ -7,12 +7,11 @@ SECRET_KEY =  os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-DATABASE_URL = f'postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}'
 
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=DATABASE_URL,
+         default=os.environ.get('DATABASE_URL', 'postgres://user:pass@localhost:5432/dbname'),
         conn_max_age=600,
         ssl_require=True
     )
--- school_portal/settings/prod.py
+++ school_portal/settings/prod.py
@@ -1,21 +1,19 @@
 # core/settings/prod.py
 from .base import *
 import os
-from dotenv import load_dotenv
 import dj_database_url
 
-load_dotenv()
 
 DEBUG = False
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'your-domain.com')]
 
-DATABASE_URL = f'postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}'
+
 
 # Example using environment variables for a real DB (Postgres)
 DATABASES = {
     'default': dj_database_url.config(
-        default=DATABASE_URL,
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
